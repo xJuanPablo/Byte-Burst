aded73397ba2852ba3d47f9f191a4bdc7570a15f
Juan Pablo Archuleta <pabloarchuleta@gmail.com>
2023-09-19 16:10:23 -0500

    Styled post structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header/Header';
 import { createContext, useState } from 'react';
+import Post from './components/Post';
 
 export const ThemeContext = createContext(null);
 
@@ -12,10 +13,16 @@ function App() {
 }
   return (
     <ThemeContext.Provider value={{theme, toggleTheme}} >
+
     <div className={`App ${theme}`}>
       <Header />
     </div>
+
+    <Post />
+
     </ThemeContext.Provider>
+
+
   );
 }
 
---
+++ client/src/components/Post.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div id="homePost">
+
+
+      <div className='post'>
+        <div className="imgContainer">
+          <img src="https://m.media-amazon.com/images/M/MV5BZjk1NTA3NjQtMmY2Ni00MjA4LWExOGItODUzZTkyZDY3MDRjXkEyXkFqcGdeQW1yb2Njbw@@._V1_QL75_UX500_CR0,0,500,281_.jpg" alt="" className='postImg'/>
+        </div>
+        <div className="content">
+          <h2 className='postH2'>Community</h2>
+          <p className='postText'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus officiis quis dolores possimus minima et! Accusamus cumque nostrum doloribus, blanditiis, atque nobis sunt, dolor cupiditate porro maxime reprehenderit obcaecati sit.</p>
+          <p className='postInfo'>
+            <a href="postAuthor" className='postAuthor'>xJuanPablo</a>
+            <time>2023-09-18</time>
+          </p>
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Post
