29be705815762a2e11c6f7d66c842410d8c9c7c2
Juan Pablo Archuleta <pabloarchuleta@gmail.com>
2023-09-20 20:03:22 -0500

    Made application home responsive to different screen sizes

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,28 +1,17 @@
 import React, {useContext} from 'react'
 import NavBar from './components/NavBar'
-import { ThemeContext } from '../../App'
 import { GiExplodingPlanet } from "react-icons/gi"
-import {BsFillCloudSunFill, BsFillMoonFill} from "react-icons/bs"
 
 function Header() {
-
-  const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
     <header>
       <div id='logoContainer divSize'>
-        <GiExplodingPlanet size={60} className='logo' id='planet'/> 
+        <GiExplodingPlanet size={60} className='logo planet' id='planet'/> 
         <h1 className='logo' id='byte'>ByteBurst</h1>
       </div>
 
-      <div className='NavBar'>
-      <NavBar/>
-      </div>
-      <div className='toggle'>
-      <button onClick={toggleTheme}>
-        {theme === 'dark' ? <BsFillCloudSunFill size={35} /> : <BsFillMoonFill size={35} />}
-      </button>
-      </div>
+      <NavBar className='NavBar'/>
     </header>
   )
 }
