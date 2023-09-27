12586785a455d9f8f1a4ce6940131d206eea0fae
Juan Pablo Archuleta <pabloarchuleta@gmail.com>
2023-09-27 11:34:10 -0500

    Set up redirects for  login and logout routes

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useContext, useState} from 'react';
+import { Link, Navigate } from 'react-router-dom'
+import { UserContext } from '../../context/UserContext';
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [toHome, setToHome] =  useState(false);
+  const {setUserInfo} = useContext(UserContext)
+
+
+  async function login(e) {
+    e.preventDefault();
+    const response = await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+      headers: {'Content-Type': 'application/json'},
+      credentials: 'include',
+    });
+    if (response.ok){
+      response.json().then(userInfo =>{
+        setUserInfo(userInfo)
+        setToHome(true);
+      });
+    } else {
+      alert('Invalid username or password')
+    }
+  }
+
+  if (toHome) {
+    return <Navigate to={'/'} />
+  }
+
   return (
     <div className='formBox'>
-      <form action="" className='loginForm'>
+      <form action="" className='login' onSubmit={login}>
         <h2>Login using your account information</h2>
-        <input type="text" placeholder='Username'/>
-        <input type="password" placeholder='password'/>
+        <input type="text" placeholder='Username' value={username} onChange={e => setUsername(e.target.value)}/>
+        <input type="password" placeholder='password' value={password} onChange={e => setPassword(e.target.value)}/>
         <button id='loginBtn'>Login</button>
 
         <p>Don't have an account? You can register <Link to="/register">Here</Link></p>
@@ -17,4 +46,4 @@ function Login() {
   )
 }
 
-export default Login
+export default Login;
--- server/index.js
+++ server/index.js
@@ -1,24 +1,73 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const mongoose = require('mongoose')
-const User = require('./models/User')
-const cors = require('cors')
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { error } = require('console');
+const cookieParser = require('cookie-parser');
+require('dotenv').config()
 
-app.use(cors());
+const salt = bcrypt.genSaltSync(10);
+const secret = process.env.SECRET_KEY;
+
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser())
 
-mongoose.connect('mongodb+srv://ByteBurst:k30x43u46R2OjU6A@cluster0.yjrqyld.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGO_DB);
 
 app.post('/register', async (req, res) => {
   const {username, password} = req.body;
   try {
-    const userInfo = await User.create({username, password})
+    const userInfo = await User.create({
+      username,
+      password:bcrypt.hashSync(password, salt)})
     res.json(userInfo);
   } catch(e){
-    res.status(400).json(e)
+    res.status(400).json(e);
   }
 })
 
-app.listen(3001)
-// ByteBurst
-//mongodb+srv://ByteBurst:k30x43u46R2OjU6A@cluster0.yjrqyld.mongodb.net/?retryWrites=true&w=majority
+app.post('/login', async (req, res) => {
+  const {username, password} = req.body;
+  try {
+  const userInfo = await User.findOne({ username });
+
+  if (!userInfo) {
+    return res.status(401).json({ error: 'Invalid username or password' });
+  }
+
+
+  const isPasswordValid = bcrypt.compareSync(password, userInfo.password);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({ error: 'Invalid username or password' });
+  } 
+
+  jwt.sign({username, id:userInfo._id}, secret,   {}, (err, token) => {
+      if(err) throw err;
+      res.cookie('token', token).json({
+        id:userInfo._id,
+        username,
+      });
+    })
+} catch (e) {
+  res.status(400).json(e);
+}
+});
+
+app.get('/profile', (req,res) =>{
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) =>{
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+app.post('logout', (req,res) => {
+  res.cookie('token', '').json('ok');
+});
+
+app.listen(4000);
